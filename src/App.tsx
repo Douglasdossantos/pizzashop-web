360c2a244720bb9eff74dc641feac4f5f10754ac
Douglas costa <douglasalgustocosta@gmail.com>
2024-01-19 01:44:25 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './global.css'
+
+import { RouterProvider } from 'react-router-dom'
+import { router } from './pages/routes'
+import {HelmetProvider,Helmet} from 'react-helmet-async'
+import {Toaster} from 'sonner'
+
+export function App() {
+  return (
+    <HelmetProvider>
+      <Helmet titleTemplate='%s | pizza.shop'/>
+      <Toaster richColors closeButton/>
+      <RouterProvider router={router}/>
+
+    </HelmetProvider>
+   )
+}
---
+++ src/pages/app/dashboard.tsx
@@ -0,0 +1,6 @@
+import {Helmet} from 'react-helmet-async'
+
+export function Dashboard()
+{
+    return <><Helmet title='Dasborad'/><h1>Dashborad</h1></>
+}
